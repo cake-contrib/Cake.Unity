18357dded93b1d2e3743be2ef24916f8604c7db6
Anatoliy Kolesnick <github@kolesnick.net>
2018-12-12 16:21:43 +0200

    FindUnityEditors draft based on file version.

---
+++ src/Cake.Unity/UnityEditorDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using Cake.Core.IO;
+
+namespace Cake.Unity
+{
+    public class UnityEditorDescriptor
+    {
+        public UnityEditorDescriptor(string version, FilePath path)
+        {
+            Version = version;
+            Path = path;
+        }
+
+        public string Version { get; }
+        public FilePath Path { get; }
+
+        public override string ToString() => $"Unity {Version} @ {Path.FullPath}";
+    }
+}
--- src/Cake.Unity/UnityExtensions.cs
+++ src/Cake.Unity/UnityExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Cake.Core;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Cake.Core;
 using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 using Cake.Unity.Platforms;
 
@@ -15,6 +20,46 @@ namespace Cake.Unity
         {
             var tool = new UnityRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
             tool.Run(context, projectPath, platform);
+        }
+
+        private static string ProgramFiles => Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+        [CakeMethodAlias]
+        [CakeAliasCategory("Build")]
+        public static IEnumerable<UnityEditorDescriptor> FindUnityEditors(this ICakeContext context)
+        {
+            string searchPattern = $"{ProgramFiles}/*Unity*/**/Editor/Unity.exe";
+
+            context.Log.Debug("Searching for available Unity Editors...");
+            context.Log.Debug("Search pattern: {0}", searchPattern);
+            IEnumerable<FilePath> candidates = context.Globber.GetFiles(searchPattern);
+
+            return
+                from candidatePath in candidates
+                let version = DetermineUnityEditorVersion(context, candidatePath)
+                where version != null
+                select new UnityEditorDescriptor(version, candidatePath);
+        }
+
+        private static string DetermineUnityEditorVersion(ICakeContext context, FilePath editorPath)
+        {
+            context.Log.Debug("Determining version of Unity Editor at path {0}...", editorPath);
+
+            var fileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(editorPath.FullPath);
+
+            var (year, stream, update) = (fileVersion.FileMajorPart, fileVersion.FileMinorPart, fileVersion.FileBuildPart);
+
+            if (year <= 0 || stream <= 0 || update < 0)
+            {
+                context.Log.Debug("Failed: file version {0} is incorrect. Expected first two parts to be positive numbers and third one to be non negative.", fileVersion);
+                return null;
+            }
+
+            string version = $"{year}.{stream}.{update}";
+
+            context.Log.Debug("Success: {0}", version);
+
+            return version;
         }
     }
 }
