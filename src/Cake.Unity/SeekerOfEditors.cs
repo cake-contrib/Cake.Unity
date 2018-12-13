2160184690d26f5a547a96b437c092841ff12546
Anatoliy Kolesnick <github@kolesnick.net>
2018-12-13 11:08:17 +0200

    Unity version suffix is now determined for Unity Hub-like folder structure.

--- src/Cake.Unity/SeekerOfEditors.cs
+++ src/Cake.Unity/SeekerOfEditors.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Cake.Core;
@@ -57,11 +58,61 @@ namespace Cake.Unity
                 return null;
             }
 
-            var version = new UnityVersion(year, stream, update);
+            log.Debug("Unity version from file version: {0}.{1}.{2}", year, stream, update);
 
-            log.Debug("Success: {0}", version);
+            var suffix = DetermineVersionSuffix(editorPath, year, stream, update);
 
-            return version;
+            if (suffix.HasValue)
+            {
+                log.Debug("Unity version suffix determined.");
+                var version = new UnityVersion(year, stream, update, suffix.Value.character, suffix.Value.number);
+                log.Debug("Result Unity Editor version (full): {0}", version);
+                return version;
+            }
+            else
+            {
+                log.Debug("Unity version suffix not determined.");
+                var version = new UnityVersion(year, stream, update);
+                log.Debug("Result Unity Editor version (short): {0}", version);
+                return version;
+            }
         }
+
+        private (char character, int number)? DetermineVersionSuffix(FilePath editorPath, int year, int stream, int update)
+        {
+            log.Debug("Determining version suffix from folder structure...");
+
+            var parentFolders =
+                editorPath
+                    .GetDirectory()
+                    .Segments
+                    .Reverse();
+
+            var suffixes =
+                Enumerable.ToList
+                (
+                    from folder in parentFolders
+                    where folder.StartsWith($"{year}.{stream}.{update}")
+                    let suffixString = folder.Remove(0, $"{year}.{stream}.{update}".Length)
+                    let suffixCharacter = suffixString[0]
+                    let suffixNumberPart = suffixString.Remove(0, 1)
+                    where IsInt(suffixNumberPart)
+                    let suffixNumber = int.Parse(suffixNumberPart)
+                    select (character: suffixCharacter, number: suffixNumber)
+                );
+
+            if (suffixes.Count == 0)
+            {
+                log.Debug("Failed: no suitable folders found. Try to install needed Editor using Unity Hub.");
+                return null;
+            }
+
+            var suffix = suffixes.First();
+            log.Debug("Determined suffix: {0}{1}", suffix.character, suffix.number);
+
+            return suffix;
+        }
+
+        private bool IsInt(string value) => int.TryParse(value, out _);
     }
 }
--- src/Cake.Unity/UnityVersion.cs
+++ src/Cake.Unity/UnityVersion.cs
@@ -9,10 +9,22 @@
             this.Update = update;
         }
 
+        public UnityVersion(int year, int stream, int update, char suffixCharacter, int suffixNumber)
+            : this(year, stream, update)
+        {
+            this.SuffixCharacter = suffixCharacter;
+            this.SuffixNumber = suffixNumber;
+        }
+
         public int Year { get; }
         public int Stream { get; }
         public int Update { get; }
+        public char? SuffixCharacter { get; }
+        public int? SuffixNumber { get; }
 
-        public override string ToString() => $"{Year}.{Stream}.{Update}";
+        public override string ToString() =>
+            SuffixCharacter.HasValue && SuffixNumber.HasValue
+                ? $"{Year}.{Stream}.{Update}{SuffixCharacter}{SuffixNumber}"
+                : $"{Year}.{Stream}.{Update}";
     }
 }
