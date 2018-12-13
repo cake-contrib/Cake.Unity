0131b94ded3f9dda9d1b26846bec685a553d54e7
Anatoliy Kolesnick <github@kolesnick.net>
2018-12-13 14:00:56 +0200

    FindUnityEditor(year), FindUnityEditor(year, stream).

--- src/Cake.Unity/UnityExtensions.cs
+++ src/Cake.Unity/UnityExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Cake.Core;
 using Cake.Core.Annotations;
 using Cake.Core.IO;
@@ -18,6 +19,36 @@ namespace Cake.Unity
             var tool = new UnityRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
             tool.Run(context, projectPath, platform);
         }
+
+        [CakeMethodAlias]
+        [CakeAliasCategory("Build")]
+        public static UnityEditorDescriptor FindUnityEditor(this ICakeContext context, int year) =>
+            Enumerable.FirstOrDefault
+            (
+                from editor in context.FindUnityEditors()
+                let version = editor.Version
+                where
+                    version.Year == year
+                orderby
+                    version.Stream descending,
+                    version.Update descending
+                select editor
+            );
+
+        [CakeMethodAlias]
+        [CakeAliasCategory("Build")]
+        public static UnityEditorDescriptor FindUnityEditor(this ICakeContext context, int year, int stream) =>
+            Enumerable.FirstOrDefault
+            (
+                from editor in context.FindUnityEditors()
+                let version = editor.Version
+                where
+                    version.Year == year &&
+                    version.Stream == stream
+                orderby
+                    version.Update descending
+                select editor
+            );
 
         [CakeMethodAlias]
         [CakeAliasCategory("Build")]
