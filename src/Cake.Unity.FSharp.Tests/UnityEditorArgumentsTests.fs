32fed5ae33d2621352859a2728d30fac353122a4
Anatoliy Kolesnick <github@kolesnick.net>
2019-01-15 10:14:37 +0200

    Added basic tests on UnityEditorArguments.

---
+++ src/Cake.Unity.FSharp.Tests/UnityEditorArgumentsTests.fs
@@ -0,0 +1,21 @@
+﻿module Cake.Unity.Tests.UnityEditorArgumentsTests
+
+open FsUnit.TopLevelOperators
+open NUnit.Framework
+open Cake.Core.IO
+open Cake.Unity
+
+let commandLineArguments (arguments : UnityEditorArguments) =
+    let apply (arguments : UnityEditorArguments) builder =
+        arguments.CustomizeCommandLineArguments (builder, null)
+    let render (builder : ProcessArgumentBuilder) = builder.Render()
+
+    () |> ProcessArgumentBuilder |> apply arguments |> render
+
+let setBatchMode value (arguments : UnityEditorArguments) =
+    arguments.BatchMode <- value
+    arguments
+
+[<Test>]
+let ``CLI arguments with enabled BatchMode should contain "-batchmode"`` () =
+    () |> UnityEditorArguments |> setBatchMode true |> commandLineArguments |> should haveSubstring "-batchmode"
