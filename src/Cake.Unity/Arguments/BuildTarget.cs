88628428fb6eb41c298b3a7cc42566091efefa00
Anatoliy Kolesnick <github@kolesnick.net>
2019-02-04 17:53:32 +0200

    added -buildTarget argument support

---
+++ src/Cake.Unity/Arguments/BuildTarget.cs
@@ -0,0 +1,27 @@
+﻿namespace Cake.Unity.Arguments
+{
+    public enum BuildTarget
+    {
+        standalone,
+        Win,
+        Win64,
+        OSXUniversal,
+        Linux,
+        Linux64,
+        LinuxUniversal,
+        iOS,
+        Android,
+        Web,
+        WebStreamed,
+        WebGL,
+        XboxOne,
+        PS4,
+        PSP2,
+        WindowsStoreApps,
+        Switch,
+        WiiU,
+        N3DS,
+        tvOS,
+        PSM,
+    }
+}
