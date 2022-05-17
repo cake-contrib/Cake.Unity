b6837ad999ebb7e92fde88ec40f614eeb0cd5859
Anatoliy Kolesnick <github@kolesnick.net>
2022-05-17 11:55:32 +0300

    inconclusive test result for Win platform

--- src/Cake.Unity.FSharp.Tests/OSXSeekerOfEditorsTests.fs
+++ src/Cake.Unity.FSharp.Tests/OSXSeekerOfEditorsTests.fs
@@ -5,17 +5,24 @@ open Cake.Core.IO
 open NUnit.Framework
 open Cake.Unity.SeekersOfEditors
 open Cake.Testing
+open System.Runtime.InteropServices
 
 let seek (seeker : OSXSeekerOfEditors) = seeker.Seek ()
+let runningOnWindows = RuntimeInformation.IsOSPlatform OSPlatform.Windows
 
 [<Test>]
 let ``seek should not fail on real file system`` () =
-    let environment = FakeEnvironment PlatformFamily.OSX
-    let fileSystem = FileSystem ()
-    let globber = Globber (fileSystem, environment)
-    let log = FakeLog ()
 
-    (environment, globber, log, fileSystem)
-    |> OSXSeekerOfEditors
-    |> seek
-    |> ignore
+    if runningOnWindows then
+        Assert.Inconclusive ()
+
+    else
+        let environment = FakeEnvironment PlatformFamily.OSX
+        let fileSystem = FileSystem ()
+        let globber = Globber (fileSystem, environment)
+        let log = FakeLog ()
+
+        (environment, globber, log, fileSystem)
+        |> OSXSeekerOfEditors
+        |> seek
+        |> ignore
